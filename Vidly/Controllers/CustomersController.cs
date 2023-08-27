72ae4ce48b0a839a0aea6d9c59c003648f367bcb
Ermyas <Ermyas@DESKTOP-KCQ8SGP>
2023-08-27 16:51:04 +0300

    ASP.NET MVC Fundamentals

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> _customers = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "Tsegay Hailemariam" },
+            new Customer { Id = 2, Name = "Haftom Abadi" },
+        };
+
+        public ActionResult Index()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek",
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = _customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,21 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Random()
+        private List<Movie> _movie = new List<Movie>
         {
-            var movie = new Movie()
+            new Movie { Id = 1, Name = "The GOD must be crazy" },
+            new Movie { Id = 2, Name = "Wakanda" },
+        };
+
+        public ActionResult Index()
+        {
+            
+
+            var movie = new Movie
             {
-                Name = "Shrek",
+                Movies = _movie,
             };
+
+            return View(movie);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _movie.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
             return View(movie);
         }
     }
